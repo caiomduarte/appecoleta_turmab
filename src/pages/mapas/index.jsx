ee848f0a4457a4b1ee5bd754ae6d4184dc6f635b
Caio Duarte <md.caio@yahoo.com.br>
2024-05-16 19:10:20 -0300

    Manipulando mapas com o React Native Maps

--- src/pages/mapas/index.jsx
+++ src/pages/mapas/index.jsx
@@ -1,12 +1,32 @@
 
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View,
+         Text,
+         TouchableOpacity,
+         ScrollView,
+         Image } from 'react-native';
+
+import { Feather } from '@expo/vector-icons';
+
+import MapView from 'react-native-maps';
 
 import styles from '../../styles';
 
 export default function Mapas() {
  return (
    <View style={styles.container}>
-        <Text>Tela de Mapas</Text>
+        <TouchableOpacity>
+             <Feather name="arrow-left-circle" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Bem vindo.</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+   
+        <View>
+            <MapView style={{width:'100%', height:'100%'}}>
+
+            </MapView>
+        </View>
+
     </View>
   );
 }
